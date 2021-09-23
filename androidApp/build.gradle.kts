60d69675d53e8458bc9d02875d61c4de4cd0247b
Zhaolong Zhong <zhlgzh@gmail.com>
2021-09-23 01:58:06 -0700

    Add Jetpack Compose

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,3 +1,5 @@
+val composeVersion = "1.0.2"
+
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -5,9 +7,18 @@ plugins {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    testImplementation("junit:junit:4.+")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 }
 
 android {
@@ -23,5 +34,19 @@ android {
         getByName("release") {
             isMinifyEnabled = false
         }
+    }
+    lint {
+        isWarningsAsErrors = true
+        isAbortOnError = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = composeVersion
+    }
+    buildFeatures {
+        compose = true
     }
 }
--- androidApp/src/main/java/com/zhaolongzhong/tiktok/android/MainActivity.kt
+++ androidApp/src/main/java/com/zhaolongzhong/tiktok/android/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.zhaolongzhong.tiktok.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zhaolongzhong.tiktok.Greeting
-import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            Text("Hello world! ${greet()}")
+        }
     }
 }
