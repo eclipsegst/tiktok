1ea49b4545c023ea868242ad4a4e417c80e03a29
Zhaolong Zhong <zhlgzh@gmail.com>
2021-09-24 21:49:34 -0700

    Add AppState

--- androidApp/src/main/java/com/zhaolongzhong/tiktok/android/MainActivity.kt
+++ androidApp/src/main/java/com/zhaolongzhong/tiktok/android/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.zhaolongzhong.tiktok.android
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.zhaolongzhong.tiktok.datalayer.Repository
 import com.zhaolongzhong.tiktok.datalayer.functions.getCountriesListData
+import com.zhaolongzhong.tiktok.viewmodel.TViewModel
 import com.zhaolongzhong.tiktok.viewmodel.screens.CountriesListState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,30 +20,39 @@ class MainActivity : ComponentActivity() {
         private const val TAG = "MainActivity"
     }
 
-    private var screenState =
-        mutableStateOf(CountriesListState(isLoading = true, countriesListItems = emptyList()))
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val model = (application as TApp).model
 
-        Log.d(TAG, "setContent")
-
         setContent {
-            CountriesListScreen(
-                countriesListState = screenState.value,
-                onListItemClick = {
-                    Log.d(TAG, "onListItemClick: $it")
-                },
-                onFavoriteIconClick = { Log.d(TAG, "onFavoriteIconClick: $it") },
-            )
+            MainComposable(model = model)
         }
+    }
+}
 
+@Composable
+fun MainComposable(model: TViewModel) {
+    val appState = AppState(model.repo)
+    val stateFlow = remember { mutableStateOf(appState) }
+
+    CountriesListScreen(
+        countriesListState = stateFlow.value.countryListScreenState.value,
+        onListItemClick = { },
+        onFavoriteIconClick = { },
+    )
+}
+
+class AppState(private val repo: Repository) {
+
+    var countryListScreenState =
+        mutableStateOf(CountriesListState(isLoading = true, countriesListItems = emptyList()))
+
+    init {
         // TODO: handle coroutine scope properly
         CoroutineScope(Job() + Dispatchers.Main).launch {
-            val result = model.repo.getCountriesListData()
-            Log.d(TAG, "Update state with list size: ${result.size}")
-            screenState.value = CountriesListState(isLoading = false, countriesListItems = result)
+            val result = repo.getCountriesListData()
+            countryListScreenState.value =
+                CountriesListState(isLoading = false, countriesListItems = result)
         }
     }
 }
