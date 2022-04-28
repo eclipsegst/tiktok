27ed2c6d4f4586dfe65142c71d8e301c74d942c3
Zhaolong Zhong <zhlgzh@gmail.com>
2022-04-27 20:48:53 -0700

    Add MeTabView

--- iosApp/iosApp/screens/MeScreen.swift
+++ iosApp/iosApp/screens/MeScreen.swift
@@ -54,7 +54,7 @@ struct MeScreen: View {
                     Image(systemName: "chevron.right").foregroundColor(.gray)
                 }.padding()
             }
-            
+            MeTabView()
             Spacer()
                 
         }.navigationBarTitle("", displayMode: .inline)
@@ -74,5 +74,76 @@ struct FollowBlock: View {
                 .fontWeight(.light)
                 .foregroundColor(.gray)
         }.frame(minWidth: 80)
+    }
+}
+
+struct MeTabView: View {
+    @State var currentTab: Int = 0
+    @Namespace private var animation
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            TabView(selection: self.$currentTab, content: {
+                view1.tag(0)
+                view2.tag(1)
+                view3.tag(2)
+            })
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .edgesIgnoringSafeArea(.all)
+            navigationBarView
+        }
+    }
+    var navigationitemsIcon: [String] = ["books.vertical", "heart", "lock"]
+    
+    var navigationBarView: some View {
+            HStack {
+                ForEach(Array(zip(self.navigationitemsIcon.indices, self.navigationitemsIcon)), id: \.0, content: {
+                    index, name in
+                    navBarItem(iconName: name, tab: index)
+                })
+            }
+            .background(Color.white)
+            .frame(height: 35)
+            .edgesIgnoringSafeArea(.top)
+            .padding(.bottom, 2)
+    }
+    
+    func navBarItem(iconName: String, tab: Int) -> some View {
+        Button {
+            self.currentTab = tab
+        } label: {
+            VStack {
+                Spacer()
+                Image(systemName: iconName)
+                    .foregroundColor(self.currentTab == tab ? Color.black : Color.gray)
+                    .frame(maxWidth: .infinity)
+                    .padding(.all, 3)
+                if self.currentTab == tab {
+                    Color.black.frame(width: 60, height: 2)
+                        .matchedGeometryEffect(id: "underline", in: animation, properties: .frame)
+                } else {
+                    Color.clear.frame(width: 60, height: 2)
+                }
+            }.animation(.spring(), value: currentTab)
+        }
+        .buttonStyle(.plain)
+    }
+
+    var view1: some View {
+        Color.orange.opacity(0.2).edgesIgnoringSafeArea(.all)
+    }
+
+    var view2: some View {
+        Color.blue.opacity(0.2).edgesIgnoringSafeArea(.all)
+    }
+    
+    var view3: some View {
+        Color.yellow.opacity(0.2).edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct MeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeTabView()
     }
 }
