6f06b78dd05fbc330389107b083a8133af525528
Zhaolong Zhong <zhlgzh@gmail.com>
2022-05-22 21:44:26 -0700

    Added StaggeredVerticalGrid

--- feature-little-red-book/src/main/java/com/zhaolongzhong/feature/little/red/book/ExploreScreen.kt
+++ feature-little-red-book/src/main/java/com/zhaolongzhong/feature/little/red/book/ExploreScreen.kt
@@ -1,16 +1,122 @@
 package com.zhaolongzhong.feature.little.red.book
 
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.math.ceil
+import kotlin.random.Random
 
+@OptIn(
+    ExperimentalFoundationApi::class,
+    ExperimentalMaterialApi::class
+)
 @Composable
 fun ExploreScreen() {
-    Text(
-        text = "Page - Explore", modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(1f)
-    )
+    val list = (1..20).map { it }
+
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        StaggeredVerticalGrid(
+            maxColumnWidth = 220.dp,
+            modifier = Modifier.padding(4.dp)
+        ) {
+            list.forEach { index ->
+                Card(
+                    backgroundColor = Color.Gray.copy(alpha = 0.6f),
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .height(height = (Random.nextInt(4, 8) * 30).dp)
+                        .fillMaxWidth(),
+                    elevation = 3.dp,
+                    onClick = {}
+                ) {
+                    Text(
+                        text = list[index - 1].toString(),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp,
+                        color = Color(0xFFFFFFFF),
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Reference:
+ * - https://github.com/android/compose-samples/tree/main/Owl
+ * - https://github.com/android/compose-samples/blob/main/Owl/app/src/main/java/com/example/owl/ui/courses/FeaturedCourses.kt
+ */
+@Composable
+fun StaggeredVerticalGrid(
+    modifier: Modifier = Modifier,
+    maxColumnWidth: Dp,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        content = content,
+        modifier = modifier
+    ) { measurables, constraints ->
+        check(constraints.hasBoundedWidth) {
+            "Unbounded width not supported"
+        }
+        val columns = ceil(constraints.maxWidth / maxColumnWidth.toPx()).toInt()
+        val columnWidth = constraints.maxWidth / columns
+        val itemConstraints = constraints.copy(maxWidth = columnWidth)
+        val colHeights = IntArray(columns) { 0 } // track each column's height
+        val placeables = measurables.map { measurable ->
+            val column = shortestColumn(colHeights)
+            val placeable = measurable.measure(itemConstraints)
+            colHeights[column] += placeable.height
+            placeable
+        }
+
+        val height = colHeights.maxOrNull()?.coerceIn(constraints.minHeight, constraints.maxHeight)
+            ?: constraints.minHeight
+        layout(
+            width = constraints.maxWidth,
+            height = height
+        ) {
+            val colY = IntArray(columns) { 0 }
+            placeables.forEach { placeable ->
+                val column = shortestColumn(colY)
+                placeable.place(
+                    x = columnWidth * column,
+                    y = colY[column]
+                )
+                colY[column] += placeable.height
+            }
+        }
+    }
+}
+
+private fun shortestColumn(colHeights: IntArray): Int {
+    var minHeight = Int.MAX_VALUE
+    var column = 0
+    colHeights.forEachIndexed { index, height ->
+        if (height < minHeight) {
+            minHeight = height
+            column = index
+        }
+    }
+    return column
 }
