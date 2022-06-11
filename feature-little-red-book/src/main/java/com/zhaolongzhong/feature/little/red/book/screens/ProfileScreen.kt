7e8ba8d31b7df7a5d3e81e0a6e33edebe20a24fc
Zhaolong Zhong <zhlgzh@gmail.com>
2022-06-10 19:31:08 -0700

    Added tabs to ProfileScreen

--- feature-little-red-book/src/main/java/com/zhaolongzhong/feature/little/red/book/screens/ProfileScreen.kt
+++ feature-little-red-book/src/main/java/com/zhaolongzhong/feature/little/red/book/screens/ProfileScreen.kt
@@ -22,12 +22,16 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,17 +41,23 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
 import com.zhaolongzhong.feature.little.red.book.R
+import kotlinx.coroutines.launch
 import me.onebone.toolbar.CollapsingToolbarScaffold
 import me.onebone.toolbar.ScrollStrategy
 import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
 import kotlin.random.Random
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun ProfileScreen() {
     TopBar()
     CollapsingToolbarScaffold(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().background(color = Color(0xFF505454)),
         state = rememberCollapsingToolbarScaffoldState(),
         scrollStrategy = ScrollStrategy.EnterAlwaysCollapsed,
         toolbar = {
@@ -61,19 +71,73 @@ fun ProfileScreen() {
             }
         }) {
         Column(modifier = Modifier) {
-            TabView()
-            TabsContent()
+            val pages = ProfileTab.values()
+            // Reference: https://google.github.io/accompanist/pager/
+            val pagerState = rememberPagerState()
+            val coroutineScope = rememberCoroutineScope()
+
+            TabRow(
+                // Our selected tab is our current page
+                selectedTabIndex = pagerState.currentPage,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .height(56.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
+                    .background(Color.White),
+                // Override the indicator, using the provided pagerTabIndicatorOffset modifier
+                // Indicator width is using default Tab width, so setting width will be override
+                // by default width, using padding to control width instead
+                indicator = { tabPositions ->
+                    TabRowDefaults.Indicator(
+                        Modifier
+                            .pagerTabIndicatorOffset(pagerState, tabPositions)
+                            .padding(start = 30.dp, end = 30.dp),
+                        color = Color.Magenta
+                    )
+                }
+            ) {
+                // Add tabs for all of our pages
+                pages.forEachIndexed { index, tab ->
+                    // Tab margin: https://stackoverflow.com/a/68951035
+                    Tab(
+                        modifier = Modifier
+                            .background(Color.White)
+                            .padding(start = 0.dp, end = 0.dp),
+                        text = {
+                            Text(
+                                text = tab.tabName,
+                                fontSize = 12.sp,
+                                color = if (pagerState.targetPage == index) Color.DarkGray else Color.LightGray,
+                                modifier = Modifier
+                                    .padding(start = 0.dp, end = 0.dp)
+                            )
+                        },
+                        selected = pagerState.currentPage == index,
+                        onClick = {
+                            coroutineScope.launch {
+                                pagerState.animateScrollToPage(index)
+                            }
+                        },
+                    )
+                }
+            }
+
+            HorizontalPager(
+                count = pages.size,
+                state = pagerState,
+            ) { page ->
+                SelectedProfileTab(ProfileTab.values()[pagerState.targetPage])
+            }
         }
     }
 }
-
 
 @Composable
 fun ProfileSection() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.LightGray)
     ) {
         ProfileImageSection()
         Text(
@@ -221,7 +285,7 @@ fun TabsContent() {
         modifier = Modifier
             .fillMaxWidth()
             .verticalScroll(rememberScrollState())
-            .background(color = Color.LightGray.copy(0.1f))
+            .background(color = Color.White)
     ) {
         repeat(30) {
             Text(text = "hello world", modifier = Modifier.height(60.dp))
@@ -251,5 +315,26 @@ private fun TopBar() {
             tint = MaterialTheme.colors.onBackground,
             modifier = Modifier.size(24.dp)
         )
+    }
+}
+
+enum class ProfileTab(val tabName: String) {
+    POST("Posts"),
+    COLLECTS("Collects"),
+    LIKED("Liked"),
+}
+
+@Composable
+fun SelectedProfileTab(tab: ProfileTab) {
+    when (tab) {
+        ProfileTab.POST -> {
+            TabsContent()
+        }
+        ProfileTab.COLLECTS -> {
+            TabsContent()
+        }
+        ProfileTab.LIKED -> {
+            TabsContent()
+        }
     }
 }
